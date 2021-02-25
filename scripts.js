d4bd194175312e037e6e2359c4f8c85377014b6f
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-02-24 21:44:16 -0300

    declarando 4 variaveis, atribuido valores e exibindo no console

--- scripts.js
+++ scripts.js
@@ -10,4 +10,10 @@ o código abaixo escreve alguma mensagem no devtools
 
 //console.log(typeof weight)
 
+let name = "Paulo"
+let age = 30
+let stars = 4.8
+isSubscribed = true
+
+console.log(`o ${name} tem ${age} anos e seu ranking é ${stars}`)
 
