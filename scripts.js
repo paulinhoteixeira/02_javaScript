c38af4c909c4fb0ae73b115cb1b71f60eea30bba
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-02-24 21:40:44 -0300

    declaração de variavel e como descobrir seu tipo

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// aqui eu tenho um comentário em linha
+
+/*aqui eu tenho um comentário de bloco
+
+o código abaixo escreve alguma mensagem no devtools
+*/
+//console.log("Bem vindos!");
+
+//let weight = 70.7
+
+//console.log(typeof weight)
+
+
