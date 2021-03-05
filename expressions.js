bb88255f723b47b65d6deec2526c36f60acff246
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-03-04 21:10:39 -0300

    utilizando new para criar objetos

--- expressions.js
+++ expressions.js
@@ -8,4 +8,18 @@
 
 */
 
+/*
 
+    new
+
+    left-hand-side expression
+    criar um novo objeto 
+*/
+
+let name = new String("Paulo")
+name.surName = "Teixeira"
+let age = new Number(31)
+console.log(name.surName, age)
+
+let date = new Date("2021-03-04")
+console.log(date)
