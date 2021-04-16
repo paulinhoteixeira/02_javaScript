f2b83f31adc409853003b2864e73bbf47a4a1cf1
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-04-15 22:12:45 -0300

    conversor de fahrenheit para celsius completo

--- celsiusEmFahrenheit.js
+++ celsiusEmFahrenheit.js
@@ -1,24 +1,29 @@
 function conversor(graus) {
-  const possuiC = graus.toUpperCase().includes("C")
-  const possuiF = graus.toLowerCase().includes("F")
+  const possuiC = graus.toUpperCase().includes("C");
+  const possuiF = graus.toUpperCase().includes("F");
 
-  if(!possuiC && !possuiF) {
-      throw new Error("Grau não identificado")
+  if (!possuiC && !possuiF) {
+    throw new Error("Grau não identificado");
   }
 
-  let formula = (farenheit) => (farenheit - 32) * 5/9
-  let grauAtualizado = ""
-  let tipoDoGrau = "C"
+  let formula = (fahrenheit) => (fahrenheit - 32) * 5/9;
+  let grauAtualizado = Number(graus.toUpperCase().replace("F", ""));
+  let tipoDoGrau = "C";
 
-  return formula(grauAtualizado) + tipoDoGrau
+  if(possuiC){
+    formula = (celsius) => celsius * 9/5 + 32;
+    grauAtualizado = Number(graus.toUpperCase().replace("C", ""));
+    tipoDoGrau = "F";
 
+  }
 
-
+  return formula(grauAtualizado) + tipoDoGrau;
 }
 
 try {
-    conversor("40F")
-    conversor("10G")
-}catch(error){
-    console.log(error.message)
+  console.log(conversor("50F"));
+  console.log(conversor("10C"));
+  conversor("10G");
+} catch (error) {
+  console.log(error.message);
 }
